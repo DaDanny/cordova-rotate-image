c25e32f89c31217989295e25154d42179513b413
Danny Francken <DanFrancken@gmail.com>
2019-03-11 12:56:16 -0700

    Added in missing Date lib

--- src/android/CordovaRotateImage.java
+++ src/android/CordovaRotateImage.java
@@ -26,6 +26,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 
